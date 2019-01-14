71e622b99d1bbfa8ffb24d0214a74956270b7da9
Ilya Strukov <iley@iley.ru>
2019-01-14 22:08:06 +0100

    make code work in lambda

--- backend/build_status.py
+++ backend/build_status.py
@@ -9,14 +9,25 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 
+DEFAULT_THING = "TrafficLightCloud"
+DEFAULT_BAMBOO_URL = "https://atlassian.aid-driving.eu/bamboo"
+DEFAULT_PLAN = "AS-AS"
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--thing", default="TrafficLightCloud",
-                        help="Name of the AWS IOT thing")
-    parser.add_argument("--bamboo", default="https://atlassian.aid-driving.eu/bamboo", help="Bamboo URL")
-    parser.add_argument("--plan", default="AS-AS", help="Bamboo build plan name")
+    parser.add_argument("--thing", default=DEFAULT_THING, help="Name of the AWS IOT thing")
+    parser.add_argument("--bamboo", default=DEFAULT_BAMBOO_URL, help="Bamboo URL")
+    parser.add_argument("--plan", default=DEFAULT_PLAN, help="Bamboo build plan name")
     args = parser.parse_args()
+    update_status(args.bamboo, args.plan, args.thing)
 
+
+def lambda_handler(event, context):
+    update_status(DEFAULT_BAMBOO_URL, DEFAULT_PLAN, DEFAULT_THING)
+
+
+def update_status(bamboo_url, bamboo_plan, thing_name):
     atlassian_user = os.getenv("ATLASSIAN_USER")
     assert atlassian_user, "ATLASSIAN_USER not specified"
     atlassian_pass = os.getenv("ATLASSIAN_PASS")
@@ -24,7 +35,7 @@ def main():
 
     auth = HTTPBasicAuth(atlassian_user, atlassian_pass)
 
-    status = get_bamboo_status(args.bamboo, args.plan, auth)
+    status = get_bamboo_status(bamboo_url, bamboo_plan, auth)
     print("last status is %s" % status)
 
     state = {"red": "off", "yellow": "off", "green": "off"}
@@ -33,7 +44,7 @@ def main():
     iot_client = boto3.client("iot-data")
     shadow = {"state": {"desired": state} }
     payload = json.dumps(shadow).encode("utf-8")
-    iot_client.update_thing_shadow(thingName=args.thing, payload=payload)
+    iot_client.update_thing_shadow(thingName=thing_name, payload=payload)
 
 
 # returns "red", "yellow" or "green"
